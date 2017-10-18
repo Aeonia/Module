6d72d268a390efddff4c74a16c894340f6e95562
Aeonia <anna.cathelineau@gmail.com>
2017-10-18 15:28:13 +0200

    premier commit

---
+++ modify-page/page-eater.js
@@ -0,0 +1,16 @@
+document.body.textContent = "";
+
+var header = document.createElement('h1');
+header.textContent = "Au boulot !";
+header.setAttribute("style", "font-size: 300%");
+//header.setAttribute("style", "text-align: center");
+document.body.appendChild(header);
+var img = document.createElement('img')
+  img.setAttribute("src","https://tse2.mm.bing.net/th?id=OIP.dj5I8lneHMh7YptLUHD5DwDIDI&pid=Api");
+  img.setAttribute("style", "width: 50vw");
+  img.setAttribute("style", "height: 50vh");
+  img.imgContent = img;
+  document.body.appendChild(img);
+
+
+;
